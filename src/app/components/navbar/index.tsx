26f13bddf2849fe6ad5e8aa39d577193d6d1648b
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-08-07 15:57:55 -0300

    navbar fix

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -10,37 +10,41 @@ export function Navbar() {
         { name: "Projetos", link: "/" },
         { name: "Equipe", link: "/" },
     ];
+
     const [open, setOpen] = useState(false);
 
     return (
-        <nav className='shadow-md w-full sticky top-0 left-0 font-viet bg-BLACK bg-opacity-90 z-50'>
+        <nav className='shadow-md w-full sticky top-0 left-0 font-viet bg-BLACK z-50'>
             <div className='md:flex items-center justify-between bg-china-black py-6 md:px-10 px-7'>
                 {/* logo section */}
                 <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
                     <img src={logo} />
                 </div>
                 {/* Menu icon */}
-                <div onClick={() => setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
+                <div onClick={() => setOpen(!open)}
+                     className='absolute right-8 top-6 cursor-pointer lg:hidden w-12 h-12 '>
                     {
-                        open ? <XMarkIcon color="white"/> : <Bars3Icon color="white"/>
+                        open ? <XMarkIcon color="white" height={48}/> : <Bars3Icon color="white" height={48}/>
                     }
                 </div>
-                {/* link items */}
-                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-gray md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
+                <ul className={`lg:flex lg:items-center lg:pb-0 pb-6 absolute max-lg:my-12 lg:static bg-BLACK bg-opacity-90 lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in-out ${open ? 'top-12' : 'top-[-490px]'}`}>
                     {
                         Links.map((link) => (
-                            <li className='md:ml-8 md:my-0 my-7 font-extralight text-2xl'>
-                                <Link to={link.link} className='text-white hover:text-yellow-400 duration-500'>{link.name}</Link>
+                            <li className='lg:ml-8 lg:my-0 my-7 font-extralight text-2xl'>
+                                {link.name !== 'Contato' ? <Link to={link.link} className='text-white
+                                hover:text-yellow-400 duration-500'>{link.name}</Link>
+                                    : <Link to={link.link} className='text-white lg:hidden
+                                hover:text-yellow-400 duration-500'>{link.name}</Link>}
                             </li>))
                     }
-                    <button className='btn bg-black text-topaz md:ml-8 font-light text-2xl rounded-2xl duration-500 md:static md:hidden py-3 px-6'>
-                    <Link to={'/login'}>
-                        Contato
-                    </Link>
-                </button>
+                    <li className='lg:ml-8 lg:my-0 my-7 font-extralight text-2xl'>
+                        <Link to={"/login"} className='text-white lg:hidden
+                        hover:text-yellow-400 duration-500'>Contato</Link>
+                    </li>
                 </ul>
                 {/* button */}
-                <button className='btn bg-black text-topaz md:ml-8 font-light text-2xl rounded-2xl duration-500 md:static max-md:hidden py-3 px-6 text-ORANGE'>
+                <button
+                    className='btn bg-black text-topaz lg:ml-8 font-light text-2xl rounded-2xl duration-500 lg:static max-lg:hidden py-3 px-6 text-ORANGE'>
                     <Link to={'/login'}>
                         Contato
                     </Link>
