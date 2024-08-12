3020dee7801c34674e1eb125d0de3418be79cd33
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-08-12 20:44:35 -0300

    finishing first version of the login screen

--- src/app/screens/client/login/index.tsx
+++ src/app/screens/client/login/index.tsx
@@ -1,38 +1,101 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ClientContext } from "../../../context/client_context";
+import LooperGroup from "../../../../assets/logo.png";
+import { FloatLabel } from 'primereact/floatlabel';
+import StarsCanvas from "../../../components/starbackground"
+import { InputText } from "primereact/inputtext";
+import 'primeicons/primeicons.css';
+        
+        
 
 
 export function Login() {
 
+    const [emailErr, setEmailErr] = useState("");
+    const [passwordErr, setPasswordErr] = useState("");
+    const [isVisible, setIsVisible] = useState(false)
     const { signIn } = useContext(ClientContext)
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
 
-    async function Login() {
+    async function RandomLogin() {
+        setEmailErr("")
+        setPasswordErr("")
+        if(email === "" && password === ""){
+            setEmailErr("Please fill all the fields")
+            setPasswordErr("Please fill all the fields")
+        } else if (email === ""){
+            setEmailErr("Please fill the email field")
+        } else if (password === ""){
+            setPasswordErr("Please fill the password field")
+        } 
+
         const response = await signIn(email, password)
         console.log(response)
     }
 
+    useEffect(() => {
+        setEmail("")
+        setPassword("")
+    }, [])
+
+
     return (
-        <main className="bg-gray-200 min-h-screen flex justify-center items-center">
-            <div className="bg-white p-8 rounded shadow-md w-80">
-                <h2 className="text-2xl font-bold mb-4">Login</h2>
-                <form onSubmit={Login}>
-                    <div className="mb-4">
-                        <label className="block text-sm font-medium text-gray-600">Email</label>
-                        <input onChange={(e) => setEmail(e.target.value)} className="w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+        <>
+            <StarsCanvas />
+            <main className="bg-black min-h-screen flex justify-center items-center" >
+                <div className="absolute bg-transparent backdrop-blur-xs border-2 border-solid p-8 rounded-2xl shadow-3xl shadow-white m-2 w-144 h-144" >
+                    <div className="flex justify-center items-center mb-20" >
+                        <img src={LooperGroup} alt="logo" />
                     </div>
-                    <div className="mb-6">
-                        <label className="block text-sm font-medium text-gray-600">Senha:</label>
-                        <input onChange = {(e) => setPassword(e.target.value)} className="w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                    </div>
-                    <button type="submit" className="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50">Login</button>
-                    <div className="text-center mt-10 ">
-                        <Link to={'/register'} className="text-blue-500 underline">Cadastre-se</Link>
-                    </div>
-                </form>
-            </div>
-        </main>
+                    <form >
+                        <div className="mb-10">
+                            <FloatLabel>
+                            <div className="relative">
+                                <InputText 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    className="pr-10 text-white w-full h-10 p-2 border-2 shadow-white shadow-sm bg-black rounded-md drop-shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
+                                    value={email} 
+                                />
+                                <div className="absolute right-3 top-1/2 transform -translate-y-1/2"><i className="pi pi-user" style={{color: 'white'}}></i></div>
+                            </div>
+                                <label className="duration-300 block text-sm font-big text-white ml-2" htmlFor="user">Email</label>
+                            </FloatLabel>
+                            <small id="user-help" className={`${emailErr ? "visible" : "hidden"} text-rose-500`}>{emailErr}</small>
+                        </div>
+                        <div className="mb-8">
+                            <FloatLabel>
+                                <div>
+                                    <InputText 
+                                        onChange={(e) => setPassword(e.target.value)} 
+                                        className="text-white w-full h-10 p-2 border-2 shadow-white shadow-sm bg-black rounded-md drop-shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
+                                        id="password" 
+                                        type={isVisible ? 'text' : 'password'}
+                                        size={30} 
+                                    />
+                                    <div onClick={() => setIsVisible(!isVisible)} className="absolute right-3 top-1/2 transform -translate-y-1/2 hover:cursor-pointer">
+                                        <i className={isVisible ? 'pi pi-eye' : 'pi pi-eye-slash'} style={{color: 'white'}}></i>
+                                    </div>
+                                </div>
+                                <label className="duration-300 block text-sm font-big text-white ml-2" htmlFor="password">Password</label>
+                            </FloatLabel>
+                            <small id="password-help" className={`${passwordErr ? "visible" : "hidden"} text-rose-500`}>{passwordErr}</small>
+                        </div>
+                        <div className="flex justify-end mb-8 mr-2">
+                            <Link to={'/register'} className="object-right text-blue-500 underline">
+                                <p>Esqueci minha senha</p>
+                            </Link>
+                        </div>
+                        <div className="flex justify-center">
+                            <button type="button" className="font-medium- mt-4 w-[30%] backdrop-blur-l border-2 shadow-white shadow-3xl text-white py-2 rounded-md duration-300 hover:bg-LIGHT_ORANGE focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50" onClick={RandomLogin}>Login</button>
+                        </div>
+                        {/* <div className="text-center mt-10 ">
+                            <p className="text-white">Don't have an account? <Link to={'/register'} className="text-blue-500 underline">Register</Link></p>
+                        </div> */}
+                    </form>
+                </div>
+            </main>
+        </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,37 @@ export default {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        '3xl': '0 0 10px rgba(255, 255 ,255, 0.2)',
+      },
+
+      backdropBlur: {
+        'xs': '2px',
+      },
       colors: {
+        'BORDER': 'rgba(255, 255, 255, 0.2)',
         'BLACK': '#161616',
         'ORANGE': '#F06B41',
+        'LIGHT_ORANGE': 'rgba(240, 107, 65, .8)',
         'BACKGROUND-DARK': '#595959',
         'B-BACKGROUND': '#060606',
         'C-BACKGROUND': '#1D1D1D',
-      }
+      },
+      spacing: {
+        '2': '0.5rem',
+        '3': '0.75rem',
+        '4': '1rem',
+        '5': '1.25rem',
+        '6': '1.5rem',
+        '7': '1.75rem',
+        '8': '2rem',
+        '112': '28rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '176': '44rem',
+        '192': '48rem',
+      },
     },
   },
   plugins: [],
