d4daf6ff5159e92a1f1c6c58f5d3f205a1eebded
João <galhardojoaopedro@gmail.com>
2024-08-14 09:44:02 -0300

    feat: Add portalInterno screen with header component and background color

---
+++ src/app/screens/portalInterno/home/index.tsx
@@ -0,0 +1,14 @@
+import Header from "../../../components/header";
+
+
+
+export function PortalInterno() {
+    return (
+        <>
+            <Header/>
+            <div className="bg-black h-screen">
+                <p>Hello Worlld</p>
+            </div>
+        </>
+    )
+}
