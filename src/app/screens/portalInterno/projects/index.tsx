33430d85961a8743f5440bede4f7da261ad4580c
João <galhardojoaopedro@gmail.com>
2024-08-14 09:44:20 -0300

    feat: Add Projects screen with header component and background color

---
+++ src/app/screens/portalInterno/projects/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../../components/header";
+
+
+
+export function Projects() {
+    return ( 
+        <>
+            <Header/>
+            <main className="bg-black h-screen">
+                <p className="text-white bg-red-500">Hello</p>
+
+            </main>
+        </>
+    )
+}
