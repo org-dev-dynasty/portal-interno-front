93ee0ddeabf4e35d924c56320eb5b612c04e7705
João <galhardojoaopedro@gmail.com>
2024-08-15 15:26:16 -0300

    feat: Add Apis screen to portalInterno

---
+++ src/app/screens/portalInterno/apis/index.tsx
@@ -0,0 +1,17 @@
+import DashboardLayout from "../../../components/header2";
+
+
+
+export function Apis() {
+    return (
+        <>
+            <DashboardLayout>
+                <div>
+                    HelloWorls
+                </div>
+            </DashboardLayout>
+            
+
+        </>
+    );
+}
