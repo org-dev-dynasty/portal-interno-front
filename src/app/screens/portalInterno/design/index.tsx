5c8f50c30d1e0f1749860ea7f0a38cfeba28e270
João <galhardojoaopedro@gmail.com>
2024-08-15 15:26:23 -0300

    feat: Add design screen to portalInterno

---
+++ src/app/screens/portalInterno/design/index.tsx
@@ -0,0 +1,18 @@
+import DashboardLayout from "../../../components/header2";
+
+
+
+
+export function Desing() {
+    return (
+        <>
+            <DashboardLayout>
+                <div>
+                    HelloWorls
+                </div>
+            </DashboardLayout>
+            
+
+        </>
+    );
+}   
